e0f628b9fee85dce984139874495a6020612409e
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-04 09:53:55 -0700

    added new stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./api/server.js");
 
 
 //heroku will add the PORT variable to the environment
-const port = provess.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
